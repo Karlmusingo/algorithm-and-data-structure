da12b46ab554b53c776df4eaaf5c2f5a4afcf0a3
Karl MUSINGO <karlemusingo@gmail.com>
2020-03-03 21:35:37 +0200

    birthday chocolate

---
+++ Birthday-Chocolate/solution.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function(inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function() {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+  let result = 0;
+  s.map((sq, i) => {
+    let day = 0;
+    for (let j = i; j < i + m; j++) {
+      day = day + s[j];
+    }
+    if (day === d) result = result + 1;
+  });
+  return result;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const s = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map(sTemp => parseInt(sTemp, 10));
+
+  const dm = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ");
+
+  const d = parseInt(dm[0], 10);
+
+  const m = parseInt(dm[1], 10);
+
+  const result = birthday(s, d, m);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
